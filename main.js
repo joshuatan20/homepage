7023d573676e512438522498f77a29bbaa05e687
joshuatan20 <joshuatan.20@outlook.com>
2023-02-13 14:32:01 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let colorArray=['#6BFFB8','#78C0E0','50B2C0','CE84AD']
 
 
 
+
 setInterval(()=>{
 
   let color = Math.floor(Math.random() * colorArray.length) 
@@ -19,5 +20,4 @@ div.style.top = top + 'px';
 div.style.left= left  +'px';
 div.style.backgroundColor = colorArray[color]
 container.appendChild(div);
-
 },100);
