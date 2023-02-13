f5f193f841e336acc58b51a0ac8010f8d68a1208
joshuatan20 <joshuatan.20@outlook.com>
2023-02-13 14:30:14 +0800

    homepage

---
+++ main.js
@@ -0,0 +1,23 @@
+import './style.css'
+
+const container = document.querySelector('.container');
+
+let colorArray=['#6BFFB8','#78C0E0','50B2C0','CE84AD']
+
+
+
+
+setInterval(()=>{
+
+  let color = Math.floor(Math.random() * colorArray.length) 
+  let left= Math.random() * window.innerWidth;
+  let top = Math.random() * window.innerHeight;
+
+  let div = document.createElement('div');
+div.className = 'circle';
+div.style.top = top + 'px';
+div.style.left= left  +'px';
+div.style.backgroundColor = colorArray[color]
+container.appendChild(div);
+
+},100);
